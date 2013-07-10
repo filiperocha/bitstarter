6caf21061ccb87c908b22ca9c6307832774a52f5
Filipe <filiperocha@gmail.com>
2013-07-10 13:47:56 +0100

    read data from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var util = require('util');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+var txt;
+var f=fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  txt=data.toString();
+});
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
