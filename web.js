feabbc98e54c5cdebfca85db3a72e9df843f4e20
Filipe <filiperocha@gmail.com>
2013-07-18 01:12:52 +0100

    update webs and edit listening port to 8080

--- web.js
+++ web.js
@@ -1,18 +1,14 @@
 var express = require('express');
-var util = require('util');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
-var txt;
-var f=fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  txt=data.toString();
-});
 app.get('/', function(request, response) {
-  response.send(txt);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
